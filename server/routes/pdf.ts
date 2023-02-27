01f80ee093cf376a1607e145a0c0910ab408a05c
Maarten van den Hooven <maartenv@kontent.ai>
2023-02-27 16:18:35 +0100

    add url param

--- server/routes/pdf.ts
+++ server/routes/pdf.ts
@@ -3,34 +3,29 @@ import puppeteer from 'puppeteer-core';
 
 const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 
-export default defineEventHandler(async () => {
+export default defineEventHandler((event) => {
+  const query = getQuery(event)
+  const url = query.url; 
   //  Open chrome
   const executablePath = await edgeChromium.executablePath || LOCAL_CHROME_EXECUTABLE
-  
+
   const browser = await puppeteer.launch({
     executablePath,
     args: edgeChromium.args,
     headless: true,
   });
-
   const page = await browser.newPage();
 
-  //  Add content
-  const html = await useStorage().getItem('assets:server:template.html');
-  await page.setContent(html, { waitUntil: 'domcontentloaded' });
+  await page.goto(url, {
+    waitUntil: 'networkidle0'
+  });
 
-  //  Set screen instead of print css
-  await page.emulateMediaType('screen');
-
-  //  Create pdf
-  const pdf = await page.pdf({
-    margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
+  const result = await page.pdf({
     format: 'a4',
-    printBackground: true
-})
-
-  // Close the browser instance
+  });
   await browser.close();
 
-  return pdf;
+  return result;
 })
+
+
