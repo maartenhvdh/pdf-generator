d13e21eb3ad8f7d42855e38771a5592215ba473c
Maarten van den Hooven <maartenv@kontent.ai>
2023-06-07 15:06:50 +0200

    contentisloaded

--- server/routes/pdf.ts
+++ server/routes/pdf.ts
@@ -3,49 +3,9 @@ import puppeteer from 'puppeteer-core';
 
 const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 
-const waitForDOMToSettle = (page, timeoutMs = 30000, debounceMs = 1000) =>
-  page.evaluate(
-    (timeoutMs, debounceMs) => {
-      let debounce = (func, ms = 1000) => {
-        let timeout;
-        return (...args) => {
-          console.log("in debounce, clearing timeout again");
-          clearTimeout(timeout);
-          timeout = setTimeout(() => {
-            func.apply(this, args);
-          }, ms);
-        };
-      };
-      return new Promise((resolve, reject) => {
-        let mainTimeout = setTimeout(() => {
-          observer.disconnect();
-          reject(new Error("Timed out whilst waiting for DOM to settle"));
-        }, timeoutMs);
- 
-        let debouncedResolve = debounce(async () => {
-          observer.disconnect();
-          clearTimeout(mainTimeout);
-          resolve();
-        }, debounceMs);
- 
-        const observer = new MutationObserver(() => {
-          debouncedResolve();
-        });
-        const config = {
-          attributes: true,
-          childList: true,
-          subtree: true,
-        };
-        observer.observe(document.body, config);
-      });
-    },
-    timeoutMs,
-    debounceMs
-  );
-
 export default defineEventHandler(async (event) => {
   const query = getQuery(event)
-  const url = query.url;
+  const url = query.url; 
   //  Open chrome
   const executablePath = await edgeChromium.executablePath || LOCAL_CHROME_EXECUTABLE
 
@@ -56,14 +16,11 @@ export default defineEventHandler(async (event) => {
   });
   const page = await browser.newPage();
 
-  // await page.goto(url, {
-  //   waitUntil: 'networkidle0'
-  // });
-
-  await page.goto(url, { 'timeout': 10000, 'waitUntil': 'domcontentloaded' });
-  await waitForDOMToSettle(page);
-
-
+  await page.goto(url, {
+    waitUntil: 'networkidle0'
+  });
+  
+  await page.waitForSelector('#contentisloaded')
   const result = await page.pdf({
     format: 'a4',
   });
